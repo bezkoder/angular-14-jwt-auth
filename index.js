86b3b7439d1c7ebf26ae3d05c1ffefdf6c27de84
jonellaho <jonel@paperclicks.net>
2022-09-02 10:37:07 +0200

    f

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('dist/angular-cloudrun'));
+app.use(express.static('dist/angular-14-jwt-auth'));
 app.get('/', function (req, res,next) {
     res.redirect('/');
 });
