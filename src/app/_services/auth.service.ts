103fbcf69bcce3b46a2231b014202c907dea199f
Jonel Laho <51534362+jonellaho@users.noreply.github.com>
2023-05-01 22:49:36 +0200

    Update auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'https://api-youthnetalbania.com/api/auth/';
+const AUTH_API = 'https://youthnetalbania.onrender.com/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
